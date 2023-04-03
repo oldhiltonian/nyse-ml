ff41135c2fed8c043981b0be526e4f96f4c448cb
unknown <oldwhpsian@gmail.com>
2023-04-03 23:34:58 +0100

    Data preprocessing

--- nyse_ml/preprocessing_tools.py
+++ nyse_ml/preprocessing_tools.py
@@ -1,19 +1,53 @@
 import pandas as pd
 import numpy as np
 
-def check_for_invalid_data(df, *args):
+def check_for_nan_columns(df):
+    """Check for columns with NaN values.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to check for NaN values.
+
+    Returns
+    -------
+    list
+        A list of columns with NaN values.
     """
-    Checks for invalid data in the dataframe.
-    
-    Checks to see if any of the columns in the dataframe contain invalid data types
-    pass by the args parameter. If the column contains invalid data, the column name
-    is appended to a list and returned.
+    return df.columns[df.isna().any()].tolist()
+
+def check_for_inf_columns(df):
+    """Check for columns with infinite values.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to check for infinite values.
+
+    Returns
+    -------
+    list
+        A list of columns with infinite values.
     """
-    if not args:
-        args = [np.inf, -np.inf, np.nan]
-    invalid_columns = []
-    for col in df.columns:
-        for value in df[col]:
-            if type(value) in args:
-                invalid_columns.append(col)
-    return invalid_columns
+    return df.columns[df.isin([np.inf, -np.inf]).any()].tolist()
+
+def drop_nan_columns(df, threshold=0.5):
+    """Drop columns with NaN values.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame to drop columns from.
+    threshold : float, optional
+        The threshold of NaN values to drop a column, by default 0.5
+
+    Returns
+    -------
+    pd.DataFrame
+        The DataFrame with columns dropped.
+    """
+    nan_columns = check_for_nan_columns(df)
+    for column in nan_columns:
+        if df[column].isna().sum() / len(df) > threshold:
+            df.drop(columns=column, inplace=True)
+    return df
--- tests/test_preprocessing_tools.py
+++ tests/test_preprocessing_tools.py
@@ -2,11 +2,15 @@ import numpy as np
 import pandas as pd
 import unittest
 from unittest.mock import Mock, patch
-from nyse_ml.preprocessing_tools import check_for_invalid_data
+from nyse_ml.preprocessing_tools import check_for_nan_columns, check_for_inf_columns, check_for_invalid_columns
 
-def test_check_for_invalid_data():
-    df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [np.inf, 2, 3]})
-    assert check_for_invalid_data(df) == ['col2']
+def test_check_for_nan_columns():
+    df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, np.nan, 6]})
+    assert check_for_nan_columns(df) == ['b']
+
+def test_check_for_inf_columns():
+    df = pd.DataFrame({'a': [1, 2, 3], 'b': [4, np.inf, 6]})
+    assert check_for_inf_columns(df) == ['b']
 
 
 if __name__ == '__main__':
